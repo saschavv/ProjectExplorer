0e2ff883a3ec2ac0289cbee3b8189538429558b8
Xander Burgerhout <x.burgerhout@tnodiana.com>
2019-06-04 16:27:54 +0200

    Improvements to server.sh startup script
    - Automatically get & initialize python virtual env if not present yet
      (e.g. after a fresh clone of the repository)
    - Allow passing additional options like --port or --help to server.sh

--- server.sh
+++ server.sh
@@ -1,3 +1,17 @@
+#!/usr/bin/bash
+scriptdir=$(dirname $0)
+
+# Create python virtual env if not there yet (e.g. after fresh clone of repo)
+if [ ! -d $scriptdir/venv ]; then
+  (cd $scriptdir; python3 -m venv venv)
+  source $scriptdir/venv/bin/activate
+  pip install -r requirements.txt
+else
+  # Just activate the python virtual env
+  source $scriptdir/venv/bin/activate
+fi
+
 export FLASK_APP=projectexplorer.py 
-python3 -m flask run --host=0.0.0.0
+# $@ allows passing --port and/or other options
+python3 -m flask run --host=0.0.0.0 "$@"
 
